acf594da6c1be849b4f80b80c061d3a4da6eba85
Oleg <eskelll4ik@gmail.com>
2021-05-23 12:30:14 +0300

    fix servises list

--- project-bus/output/gulpfile.js
+++ project-bus/output/gulpfile.js
@@ -1,14 +1,15 @@
-const { src, dest } = require('gulp');
-
-function streamTask() {
-  return src('*.js')
-    .pipe(dest('output'));
-}
-
-exports.default = streamTask;
 var gulp = require('gulp');
 var sass = require('gulp-sass');
- //и что-то выведем в консоль для подтверждения
-gulp.task('default', function(){
- console.log("something");
+
+gulp.task('sass', function(){
+  return gulp.src('./src/main.scss')
+  .pipe(sass().on('error', sass.logError))
+  .pipe(gulp.dest('./public/stylesheet/'));
+});
+gulp.task('watch', function(){
+  gulp.watch('./src/**/*.scss', ['sass']);
+});
+//gulp sass:watch
+gulp.task('watch', function() {
+    gulp.watch('./src/**/*.scss', gulp.parallel('sass'));
 });
